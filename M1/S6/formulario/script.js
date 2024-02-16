e6493851678a364b0502f4708d3c976d12d0c3e9
Heloise <heloiseapereira@gmail.com>
2024-02-16 17:49:17 -0300

    fix formulario

--- M1/S6/formulario/script.js
+++ M1/S6/formulario/script.js
@@ -1,33 +1,47 @@
-function validaNome() {
-    let nome = document.querySelector("nome")
-}
-
 function validaEmail() {
-    let email = document.querySelector("email")
+    let email = document.querySelector("#email")
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    if(email.value && !re.test(email)){
+        alert("Email invalido")
+    }
+
 }
 
 function validaSenha() {
-    let senha = document.querySelector("senha")
+    let senha = document.querySelector("#senha")
     const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
-    return re.test(password);
+    if(senha.value && !re.test(senha)){
+        alert("Senha inválida")
+    }
 }
 
-function validaSenhaVal() {
-    
+function validaSenhaRepetida() {
+    let senha = document.querySelector("#senha")
+    let senhaRepetida = document.querySelector("#senhaRepetida")
+    if (senhaRepetida && senha!==senhaRepetida) {
+        alert("Senhas não correspondem")
+    }
 }
 
-function validaDataNasci() {
-    
+function temFilhos() {
+    let filhos = document.querySelector("#filhos")
+    console.log(filhos)
+    let numFilhos = document.querySelector("#numFilhos")
+    if (filhos.checked){
+        numFilhos.disabled = false
+        numFilhos.required = true
+    }
+    else {
+        numFilhos.disabled = true
+        numFilhos.required = false
+    }
 }
 
-function filhos() {
-
-}
-form.addEventListener(
-    "submit",
-    function (event) {
+let form = document.querySelector("#formulario")
+if (form) {
+    form.addEventListener("submit", function (event) {
         event.preventDefault()
     }
 )
+    
+}
