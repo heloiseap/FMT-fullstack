87f36aa13100d510e77f31614cf8b51cc511df26
Heloise <heloiseapereira@gmail.com>
2024-02-29 16:14:31 -0300

    botao add funcionando

--- M1/todo list/script.js
+++ M1/todo list/script.js
@@ -1,17 +1,41 @@
-let item1 = document.querySelector("#item1")
-let label1 = document.querySelector("#label1")
+// let item1 = document.querySelector("#item1")
+// let label1 = document.querySelector("#label1")
 
-item1.addEventListener("change", function(){
-    if (item1.checked) {
-        label1.style.textDecorationLine = "line-through"
+// item1.addEventListener("change", function(){
+//     if (item1.checked) {
+//         label1.style.textDecorationLine = "line-through"
+//     }
+//     else{
+//         label1.style.textDecorationLine = "none"
+//     }
+
+// })
+let listaTarefas = ["Estudar HTML e CSS","Estudar JS","Fazer exercícios"]
+
+let componentes = document.querySelectorAll("div .items input")
+componentes.forEach(elemento => elemento.addEventListener("change", function(){
+    let label =document.querySelector("label[for=" + elemento.id + "]")
+
+    if (elemento.checked) {
+        label.style.textDecorationLine = "line-through"
+    }
+    else {
+        label.style.textDecorationLine = "none"
+    }
+}))
+
+let btnAddTarefa = document.querySelector("#btn-add")
+btnAddTarefa.addEventListener("click", function() {
+    let novaTarefa = document.querySelector("#add-item").value
+    if (novaTarefa!="") {
+        if (!listaTarefas.includes(novaTarefa)) {
+            listaTarefas.push(novaTarefa)
+        }
+        else {
+            alert("Tarefa já adicionada") //fazer balaozinho
+        }
     }
     else{
-        label1.style.textDecorationLine = "none"
+        alert("Campo vazio")
     }
-
 })
-
-
-
-
-
