933e204ee8bffeaab59d8940e441c2f86cb63537
Heloise <heloiseapereira@gmail.com>
2024-03-13 11:09:05 -0300

    projeto ng line trought n funcionando (ref error)

--- M1/todo list/script.js
+++ M1/todo list/script.js
@@ -1,52 +1,42 @@
-// let item1 = document.querySelector("#item1")
-// let label1 = document.querySelector("#label1")
-
-// item1.addEventListener("change", function(){
-//     if (item1.checked) {
-//         label1.style.textDecorationLine = "line-through"
-//     }
-//     else{
-//         label1.style.textDecorationLine = "none"
-//     }
-
-// })
 let listaTarefas = ["Estudar HTML e CSS", "Estudar JS", "Fazer exercícios"]
 window.onload = mostrarListaTarefas
 
 function mostrarListaTarefas() {
-    listaTarefas.forEach(tarefa => {
-        let idTarefa = listaTarefas.indexOf(tarefa)
+    let divToDoList = document.querySelector("#todo-list")
+    divToDoList.innerHTML = ""
 
-        let divTarefa = document.createElement("div")
+    listaTarefas.forEach(tarefa => {
+        const idTarefa = listaTarefas.indexOf(tarefa)
+
+        const divTarefa = document.createElement("div")
         divTarefa.classList.add("items")
-        
-        let btnCheck = document.createElement("input")
+
+        const btnCheck = document.createElement("input")
         btnCheck.type = "checkbox"
         btnCheck.id = "check-" + idTarefa
         btnCheck.classList.add("item")
-         
-        let lblTarefa = document.createElement("label")
+
+
+        const lblTarefa = document.createElement("label")
         lblTarefa.htmlFor = "check-" + idTarefa
         lblTarefa.innerHTML = tarefa
         lblTarefa.classList.add("item")
 
         btnExcluir = document.createElement("button")
-        btnExcluir.innerHTML = "<i class='bi bi-trash'></i>"        
+        btnExcluir.id = "btnCheck-" + idTarefa
+        btnExcluir.innerHTML = "<i class='bi bi-trash'></i>"
 
 
         document.querySelector("#todo-list").appendChild(divTarefa)
         divTarefa.appendChild(btnCheck)
         divTarefa.appendChild(lblTarefa)
         divTarefa.appendChild(btnExcluir)
-    })
+    }
+    )
+    
 }
-
-
-
-
-
-let componentes = document.querySelectorAll("div .items input")
-componentes.forEach(elemento => elemento.addEventListener("change", function () {
+const tarefas = document.querySelectorAll(".item") 
+tarefas.forEach(elemento => elemento.addEventListener("change", function () {
     let label = document.querySelector("label[for=" + elemento.id + "]")
 
     if (elemento.checked) {
@@ -57,8 +47,10 @@ componentes.forEach(elemento => elemento.addEventListener("change", function () 
     }
 }))
 
+//adicionar tarefas
 let btnAddTarefa = document.querySelector("#btn-add")
 btnAddTarefa.addEventListener("click", function () {
+
     let novaTarefa = document.querySelector("#add-item").value
     if (novaTarefa != "") {
         if (!listaTarefas.includes(novaTarefa)) {
@@ -66,6 +58,7 @@ btnAddTarefa.addEventListener("click", function () {
             console.log(listaTarefas)
             let inputTarefa = document.querySelector("#add-item")
             inputTarefa.value = ""
+            mostrarListaTarefas()
 
         }
         else {
