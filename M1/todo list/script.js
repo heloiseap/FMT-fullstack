cc954e53df7295e728a7b23c3675e8bc2674bb7f
Heloise <heloiseapereira@gmail.com>
2024-03-21 14:44:57 -0300

    pegando do localstorage

--- M1/todo list/script.js
+++ M1/todo list/script.js
@@ -1,125 +1,129 @@
-import tarefas from "./tarefas.json" assert {type: 'json'}
-let listaTarefas = tarefas.tarefas
-
-window.onload = mostrarListaTarefas
+// import tarefas from "./tarefas.json" assert {type: 'json'}
+// let listaTarefas = tarefas.tarefas
+let listaTarefas = JSON.parse(localStorage.getItem("listaTarefas"));
+window.onload = mostrarListaTarefas;
 
 function mostrarListaTarefas() {
-    let divToDoList = document.querySelector("#todo-list")
-    divToDoList.innerHTML = ""
+  let divToDoList = document.querySelector("#todo-list");
+  divToDoList.innerHTML = "";
 
-    listaTarefas.forEach(tarefa => {
-        let idTarefa = listaTarefas.indexOf(tarefa)
+  listaTarefas.forEach((tarefa) => {
+    let idTarefa = listaTarefas.indexOf(tarefa);
 
-        let divTarefa = document.createElement("div")
-        divTarefa.classList.add("items")
+    let divTarefa = document.createElement("div");
+    divTarefa.classList.add("items");
 
-        let btnCheck = document.createElement("input")
-        btnCheck.type = "checkbox"
-        btnCheck.id = "check-" + idTarefa
-        btnCheck.checked = tarefa.concluido
-        btnCheck.classList.add("item")
+    let btnCheck = document.createElement("input");
+    btnCheck.type = "checkbox";
+    btnCheck.id = "check-" + idTarefa;
+    btnCheck.checked = tarefa.concluido;
+    btnCheck.classList.add("item");
 
-        btnCheck.addEventListener("change", function () {
-            checkTarefa(idTarefa)
-        })
+    btnCheck.addEventListener("change", function () {
+      checkTarefa(idTarefa);
+    });
 
-        let lblTarefa = document.createElement("label")
-        lblTarefa.htmlFor = "check-" + idTarefa
-        lblTarefa.innerHTML = tarefa.titulo
-        lblTarefa.classList.add("item")
-        lblTarefa.id = "lbl-" + idTarefa
-        lblTarefa.style.textDecoration = (tarefa.concluido == true ? "line-through" : "none")
+    let lblTarefa = document.createElement("label");
+    lblTarefa.htmlFor = "check-" + idTarefa;
+    lblTarefa.innerHTML = tarefa.titulo;
+    lblTarefa.classList.add("item");
+    lblTarefa.id = "lbl-" + idTarefa;
+    lblTarefa.style.textDecoration =
+      tarefa.concluido == true ? "line-through" : "none";
 
-        let btnExcluir = document.createElement("button")
-        btnExcluir.id = "btnCheck-" + idTarefa
-        btnExcluir.innerHTML = "<i class='bi bi-trash'></i>"
+    let btnExcluir = document.createElement("button");
+    btnExcluir.id = "btnCheck-" + idTarefa;
+    btnExcluir.innerHTML = "<i class='bi bi-trash'></i>";
 
-        btnExcluir.addEventListener("click", function () {
-            let confirma = window.confirm("Deseja excluir a tarefa selecionada?")
-            if (confirma){excluirTarefa(idTarefa)}
-        })
+    btnExcluir.addEventListener("click", function () {
+      let confirma = window.confirm("Deseja excluir a tarefa selecionada?");
+      if (confirma) {
+        excluirTarefa(idTarefa);
+      }
+    });
 
-        document.querySelector("#todo-list").appendChild(divTarefa)
-        divTarefa.appendChild(btnCheck)
-        divTarefa.appendChild(lblTarefa)
-        divTarefa.appendChild(btnExcluir)
-    })
+    document.querySelector("#todo-list").appendChild(divTarefa);
+    divTarefa.appendChild(btnCheck);
+    divTarefa.appendChild(lblTarefa);
+    divTarefa.appendChild(btnExcluir);
+  });
 
-    contagemTarefas()
-
+  contagemTarefas();
 }
 
 //evento de apertar na checkbox
 function checkTarefa(idTarefa) {
-    let tarefa = listaTarefas[idTarefa]
-    let lblTarefa = document.querySelector("#lbl-"+idTarefa)
+  let tarefa = listaTarefas[idTarefa];
+  let lblTarefa = document.querySelector("#lbl-" + idTarefa);
 
-    if (tarefa.concluido == false) {
-        tarefa.concluido = true
-        lblTarefa.style.textDecoration = "line-through"
+  if (tarefa.concluido == false) {
+    tarefa.concluido = true;
+    lblTarefa.style.textDecoration = "line-through";
+    localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
+    
     } else {
-        tarefa.concluido = false
-        lblTarefa.style.textDecoration = "none"
-    }
+    tarefa.concluido = false;
+    lblTarefa.style.textDecoration = "none";
+    localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
+
+  }
 }
 
 //excluir
 function excluirTarefa(idtarefa) {
-    listaTarefas.splice(idtarefa,1)
-    localStorage.setItem("listaTarefas",JSON.stringify(listaTarefas))
-    mostrarListaTarefas()
+  listaTarefas.splice(idtarefa, 1);
+  localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
+  mostrarListaTarefas();
 }
 
 //adicionar tarefas
-function adicionarTarefa(novaTarefa){
-    if (!!novaTarefa) {
-        let tarefa = {
-            titulo: novaTarefa,
-            concluido: false
-        }
+function adicionarTarefa(novaTarefa) {
+  if (!!novaTarefa) {
+    let tarefa = {
+      titulo: novaTarefa,
+      concluido: false,
+    };
 
-        if (listaTarefas.filter(item=>item.titulo==novaTarefa).length==0) {
-            // listaTarefas.push(tarefa)
-            listaTarefas.push(tarefa)
-            localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas))
-            //limpa a tela e mostra lista atualizada:
-            let inputTarefa = document.querySelector("#add-item")
-            inputTarefa.value = ""
-            mostrarListaTarefas()
-        } else {
-            alert("Tarefa já adicionada") //fazer balaozinho
-        }
+    if (listaTarefas.filter((item) => item.titulo == novaTarefa).length == 0) {
+      // listaTarefas.push(tarefa)
+      listaTarefas.push(tarefa);
+      localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
+      //limpa a tela e mostra lista atualizada:
+      let inputTarefa = document.querySelector("#add-item");
+      inputTarefa.value = "";
+      mostrarListaTarefas();
     } else {
-        alert("Campo vazio")
-    }   
+      alert("Tarefa já adicionada"); //fazer balaozinho
+    }
+  } else {
+    alert("Campo vazio");
+  }
 }
 
-
-let btnAddTarefa = document.querySelector("#btn-add")
+let btnAddTarefa = document.querySelector("#btn-add");
 btnAddTarefa.addEventListener("click", function () {
-    let novaTarefa = document.querySelector("#add-item").value
-    adicionarTarefa(novaTarefa)
-    // if (!!novaTarefa) {
-    //     if (!listaTarefas.includes(novaTarefa)) {
-    //         listaTarefas.push([novaTarefa, false])
-    //         let inputTarefa = document.querySelector("#add-item")
-    //         inputTarefa.value = ""
-    //         mostrarListaTarefas()
+  let novaTarefa = document.querySelector("#add-item").value;
+  adicionarTarefa(novaTarefa);
+  // if (!!novaTarefa) {
+  //     if (!listaTarefas.includes(novaTarefa)) {
+  //         listaTarefas.push([novaTarefa, false])
+  //         let inputTarefa = document.querySelector("#add-item")
+  //         inputTarefa.value = ""
+  //         mostrarListaTarefas()
 
-    //     } else {
-    //         alert("Tarefa já adicionada") //fazer balaozinho
-    //     }
-    // } else {
-    //     alert("Campo vazio")
-    // }
-})
+  //     } else {
+  //         alert("Tarefa já adicionada") //fazer balaozinho
+  //     }
+  // } else {
+  //     alert("Campo vazio")
+  // }
+});
 
 //numero de tarefas
 function contagemTarefas() {
-    let divContagem = document.querySelector("#quantidade-tarefas")
-    divContagem.innerHTML = ""
-    let textoContagem = document.createElement("span")
-    textoContagem.innerHTML ="Quantidade de tarefas: " + listaTarefas.length
-    divContagem.appendChild(textoContagem)
-
+  let divContagem = document.querySelector("#quantidade-tarefas");
+  divContagem.innerHTML = "";
+  let textoContagem = document.createElement("span");
+  textoContagem.innerHTML = "Quantidade de tarefas: " + listaTarefas.length;
+  divContagem.appendChild(textoContagem);
 }
