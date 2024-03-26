ed799a1b49f5ff665c7c516af814b7ad0c0a5d38
Heloise <heloiseapereira@gmail.com>
2024-03-26 09:51:56 -0300

    s9e3

--- M1/keep health/KeepHealth/src/app/login/login.component.ts
+++ M1/keep health/KeepHealth/src/app/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  login = {
+    emailUser: '',
+    senhaUser: ''
+  }
+
+  constructor(private router: Router) {} //p usar redirecionamento
+
+  entrar() {}
+  esqueciSenha(){}
+  cadastrar() {}
 }
