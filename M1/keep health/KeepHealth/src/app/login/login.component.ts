50d66d405f114a9c6db92632dc34a02e34df06c0
Heloise <heloiseapereira@gmail.com>
2024-03-27 12:58:07 -0300

    pegando do localstorage

--- M1/keep health/KeepHealth/src/app/login/login.component.ts
+++ M1/keep health/KeepHealth/src/app/login/login.component.ts
@@ -18,40 +18,41 @@ export class LoginComponent {
   constructor(private router: Router) {} //p usar redirecionamento
 
   // TODO: importar local storage
-  // listaUsers = JSON.parse(localStorage.getItem("listaUsers"));
 
   listaUsers = [
     {
-      email: 'oi',
-      senha: 'oi',
+      email: 'admin@email.com',
+      senha: '1234',
     },
-    {
-      email: 'tchau',
-      senha: 'tchau'
-    }
-
   ]; //importar from local storage
 
   entrar() {
-    let usuario = this.listaUsers.filter((user) => user.email === this.login.emailUser);
-    if (this.login.emailUser && this.login.senhaUser
-        && usuario[0].senha === this.login.senhaUser  ) {
-            this.router.navigate(['/home']);
+    localStorage.setItem('listaUsers', JSON.stringify(this.listaUsers));
+    // let listaUsers = JSON.parse(localStorage.getItem('listaUsers'));
+    let usuario = this.listaUsers.filter(
+      (user) => user.email === this.login.emailUser
+    );
+    if (
+      this.login.emailUser &&
+      this.login.senhaUser &&
+      usuario[0].senha === this.login.senhaUser
+    ) {
+      this.router.navigate(['/home']);
     } else {
-      alert("Usuário ou senha invalidos")
+      alert('Usuário ou senha invalidos');
     }
   }
   esqueciSenha() {
+    let usu = this.listaUsers.filter(
+      (user) => user.email === this.login.emailUser
+    );
+    console.log(usu);
+    usu[0].senha = 'a1b2c4d4';
 
-    let usu =this.listaUsers.filter((user) => user.email === this.login.emailUser)
-    console.log(usu)
-    usu[0].senha = "a1b2c4d4"
-
-    alert('Utilize a senha "a1b2c4d4"')
-  
+    alert('Utilize a senha "a1b2c4d4"');
   }
 
   cadastrar() {
-    this.router.navigate(['/cadastro'])
+    this.router.navigate(['/cadastro']);
   }
 }
